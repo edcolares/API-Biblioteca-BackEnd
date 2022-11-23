2352941623a0bcb6d4cdfa2aa995ea90be7b1126
Alexsandro Tirloni <tirloni.rs@gmail.com>
2022-11-23 15:56:37 -0300

    authMiddleware - ajuste

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -10,16 +10,16 @@ interface TokenPayload{
 export default function authMiddleware(req: Request, res: Response, next: NextFunction){
     const { authorization } = req.headers
     if (!authorization) {
-        return res.sendStatus(401)        
+        return res.sendStatus(401).json({message:"Não autorizado"})        
     }
     const token = authorization.replace('Bearer', '').trim()
     try {
-        const data = jwt.verify(token, 'secret')
+        const data = jwt.verify(token, process.env.JWT_PASS ?? '')
         const { id } = data as TokenPayload
         req.userId = id
 
         return next()        
     } catch {
-        return res.sendStatus(401) 
+        return res.sendStatus(401).json({message:"Não autorizado"}) 
     }
 }
